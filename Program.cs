1a4e47861202123c801aa6194b01f210beaf023d
mariacfs15 <fernandesmc1@hotmail.com>
2022-03-23 14:30:50 -0300

    Abstraindo um Jogo de RPG Usando Orientação a Objetos com C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using rpg.src.Entities;
+
+namespace rpg 
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(wizard.Attack(1));
+        }
+    }
+}
